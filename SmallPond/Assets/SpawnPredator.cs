0609cec0322d45ca36463f7c49a8c7c099947273
k_atej <jacksonke@hendrix.edu>
2025-02-03 20:58:33 -0600

    predators spawn in frame & move right

--- SmallPond/Assets/SpawnPredator.cs
+++ SmallPond/Assets/SpawnPredator.cs
@@ -4,15 +4,32 @@ public class SpawnPredator : MonoBehaviour
 {
    
     public GameObject predator; // This should randomly set the rotation of the predator (as detailed in Swim.cs) as well as its location (offscreen on the side corresponding to its rotation)
-   
+   // public float timerLength;
+    //float timer;
     void Start()
     {
-        
+        //timer = 0f;
+        InvokeRepeating("Spawn", 1.5f, 2.0f); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //timer += Time.deltaTime;
+        //if(timer > timerLength) {
+          //  Instantiate(predator, new Vector2(Random.Range(0, 3f), Random.Range(0, 3f)), Quaternion.identity);
+            //Debug.Log("Instantiating Predator");
+            //timer = 0f;
+       // } 
     }
+
+    void Spawn() {
+        Instantiate(predator, new Vector2(Random.Range(0, 3f), Random.Range(0, 3f)), Quaternion.identity);
+        Debug.Log("Instantiating Predator");
+    }
+
+
+    
+ 
+
 }
--- SmallPond/Assets/Swim.cs
+++ SmallPond/Assets/Swim.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using UnityEngine;
 
 public class swim : MonoBehaviour
@@ -6,12 +7,19 @@ public class swim : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("Move", 0.25f, 0.25f);   
     }
 
+    public float speed;
     // Update is called once per frame
     void Update()
     {
         // if the rotation is 0, it should move from left to right, 90 moves top to bottom, 180 right to left, -90 from bottom to top
+        
+    }
+
+    void Move(){
+        
+        transform.Translate(new UnityEngine.Vector2(1, 0));
     }
 }
