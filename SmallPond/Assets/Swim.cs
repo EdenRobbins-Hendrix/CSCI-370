f1e7d2d8d172cc305eb4d4eb1295d075bdaff2fb
k_atej <jacksonke@hendrix.edu>
2025-02-06 12:46:15 -0600

    trigger adjustments ii

--- SmallPond/Assets/Swim.cs
+++ SmallPond/Assets/Swim.cs
@@ -7,6 +7,8 @@ public class swim : MonoBehaviour
     public int timer;
     private bool recentlyMade;
     public bool active;
+    private int frames;
+    private bool triggered;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,27 +25,37 @@ public class swim : MonoBehaviour
         if (timer > 500) {
             recentlyMade = false;
         }
+        frames++;
+        if (frames % 6 == 0) { //If the remainder of the current frame divided by 10 is 0 run the function.
+            triggered = false;
+    }
         // if the rotation is 0, it should move from left to right, 90 moves top to bottom, 180 right to left, -90 from bottom to top
         Move();}
-    }
 
+
+    }
+    
     void Move(){
         if (!recentlyMade && (transform.position.x > 12 || transform.position.x < -12 || transform.position.y > 7 || transform.position.y < -7)){
             Destroy(predator);
         }
         else{
             transform.Translate(new UnityEngine.Vector2(0.01f, 0));
+
         }
     }
 
+    
+
     void OnTriggerEnter2D(Collider2D collider) {
-        Debug.Log("Triggered");
+        //Debug.Log("Triggered");
 		GameObject otherObject = collider.gameObject;
-		if (otherObject.name == "Player"){
+		if (otherObject.name == "Player" && triggered){
                 GameOver gameOver = (GameOver)GameObject.FindWithTag("GameController").GetComponent("GameOver");
                 gameOver.EndGame();
-                Debug.Log("Predator triggered with: " + otherObject);
+                //Debug.Log("Predator triggered with: " + otherObject);
         }
+    triggered = true;
     }
 
 }
